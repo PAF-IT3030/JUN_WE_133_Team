d0a9b293f67c5f1563b8bde97d8f2883961bfded
Lakshan <nipunajith235@gmail.com>
2024-04-20 21:53:53 +0530

    third commit

--- src/main/java/com/PAF/rest/Repo/UserRepo.java
+++ src/main/java/com/PAF/rest/Repo/UserRepo.java
@@ -3,6 +3,6 @@ package com.PAF.rest.Repo;
 import com.PAF.rest.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository<User, Long> {
+public interface UserRepo extends JpaRepository<posts, Long> {
 
 }
