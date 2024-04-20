090f51ec3686f84e53f4694e0bf9dac6b9e441ea
Lakshan <nipunajith235@gmail.com>
2024-04-20 11:01:07 +0530

    first commit

---
+++ src/main/java/com/PAF/rest/Controller/ApiControllers.java
@@ -0,0 +1,60 @@
+package com.PAF.rest.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.PAF.rest.Models.User;
+import com.PAF.rest.Repo.UserRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@RestController
+public class ApiControllers {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping(value = "/")
+    public String getPage() {
+        return "Welcome";
+    }
+
+    @GetMapping(value = "/users")
+    public List<User> getUsers(){
+        return userRepo.findAll();
+    }
+
+    @PostMapping(value = "/save")
+    public String saveUser(@RequestBody User user){
+        userRepo.save(user);
+        return "Saved...";
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public String updateUser(@PathVariable long id, @RequestBody User user) {
+        User updatedUser = userRepo.findById(id).get();
+        updatedUser.setFirstname(user.getFirstname());
+        updatedUser.setLastname(user.getLastname());
+        updatedUser.setOccupation(user.getOccupation());
+        updatedUser.setAge(user.getAge());
+        userRepo.save(updatedUser);
+        return "Updated";
+        
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public String deleteUser(@PathVariable long id){
+        User deleteUser = userRepo.findById(id).get();
+        userRepo.delete(deleteUser);
+        return "Deleted :"+id;
+    }
+}
